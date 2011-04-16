bd89383ef368490731a095a6adc9a204619b01db
fachrial <fachrial@yahoo.com>
2011-04-16 16:05:37 +0700

    Add Master/MasterAreaRegistration.cs
    Signed-off-by: fachrial <fachrial@yahoo.com>

---
+++ app/YTech.Ltr.Web.Controllers/Master/MasterAreaRegistration.cs
@@ -0,0 +1,21 @@
+﻿using System.Web.Mvc;
+
+namespace YTech.Ltr.Web.Controllers.Master
+{
+    public class MasterAreaRegistration : AreaRegistration
+    {
+        public override string AreaName
+        {
+            get { return "Master"; }
+        }
+
+        public override void RegisterArea(System.Web.Mvc.AreaRegistrationContext context)
+        {
+            context.MapRoute(
+                    "Master_default",
+                    "Master/{controller}/{action}/{id}",
+                    new { action = "Index", id = UrlParameter.Optional }
+                );
+        }
+    }
+}
